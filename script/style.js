1629a90f69b7bddd7827b31cbb5e9721911277a0
Zyxel <zyxel@TWPCNT03103-01.local>
2019-09-17 16:31:02 +0800

    nav

---
+++ script/style.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	// var nav = $('nav');
+	// $(window).scroll(function () {
+	// 	if ($(this).scrollTop() > 120) {
+	// 		nav.addClass("small");
+	// 	} else {
+	// 		nav.removeClass("small");
+	// 	}
+	// });
+});
+
