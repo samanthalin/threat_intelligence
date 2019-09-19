70eea57bb962e00e64ca8bdafc52a10cdfe9a73e
Zyxel <zyxel@TWPCNT03103-01.local>
2019-09-19 18:14:15 +0800

    table

--- script/style.js
+++ script/style.js
@@ -1,11 +1,39 @@
 $(document).ready(function(){
-	// var nav = $('nav');
-	// $(window).scroll(function () {
-	// 	if ($(this).scrollTop() > 120) {
-	// 		nav.addClass("small");
-	// 	} else {
-	// 		nav.removeClass("small");
-	// 	}
-	// });
+	$(function(){
+	  // 預設顯示第一個 Tab
+	  var _showTab = 0;
+	  var $defaultLi = $('ul.sub_menu li').eq(_showTab).addClass('active');
+	  $($defaultLi.find('a').attr('href')).siblings().hide();
+	 
+	  // 當 li 頁籤被點擊時...
+	  // 若要改成滑鼠移到 li 頁籤就切換時, 把 click 改成 mouseover
+	  $('ul.sub_menu li').click(function() {
+	    // 找出 li 中的超連結 href(#id)
+	    var $this = $(this),
+	      _clickTab = $this.find('a').attr('href');
+	    // 把目前點擊到的 li 頁籤加上 .active
+	    // 並把兄弟元素中有 .active 的都移除 class
+	    $this.addClass('active').siblings('.active').removeClass('active');
+	    // 淡入相對應的內容並隱藏兄弟元素
+	    $(_clickTab).stop(false, true).fadeIn().siblings().hide();
+	 
+	    return false;
+	  }).find('a').focus(function(){
+	    this.blur();
+	  });
+	});
+
+	$(function(){
+
+	});
 });
 
+$(document).ready(function(){
+    $('#EncyTable').dataTable({
+    	"scrollX": true,
+    	"lengthChange": false,
+    	"ordering": false,
+    	"searching": false,
+    	"pageLength": 20
+    });
+});
